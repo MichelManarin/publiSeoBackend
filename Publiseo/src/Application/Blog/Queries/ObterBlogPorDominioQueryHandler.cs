2259bb91bb65e349c83294813d2603206988a30f
Michel Manarin <michelmanarin@gmail.com>
2026-03-11 16:57:36 -0300

    fix: ajuste

--- Publiseo/src/Application/Blog/Queries/ObterBlogPorDominioQueryHandler.cs
+++ Publiseo/src/Application/Blog/Queries/ObterBlogPorDominioQueryHandler.cs
@@ -1,6 +1,7 @@
 using Application.Blog.Contracts;
 using Domain.Interfaces.Repositories;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Application.Blog.Queries;
@@ -9,12 +10,19 @@ namespace Application.Blog.Queries;
 public sealed class ObterBlogPorDominioQueryHandler : IRequestHandler<ObterBlogPorDominioQuery, BlogPorDominioResponse?>
 {
     private readonly IBlogDominioRepository _blogDominioRepository;
+    private readonly ILogger<ObterBlogPorDominioQueryHandler> _logger;
 
-    public ObterBlogPorDominioQueryHandler(IBlogDominioRepository blogDominioRepository)
-        => _blogDominioRepository = blogDominioRepository;
+    public ObterBlogPorDominioQueryHandler(
+        IBlogDominioRepository blogDominioRepository,
+        ILogger<ObterBlogPorDominioQueryHandler> logger)
+    {
+        _blogDominioRepository = blogDominioRepository;
+        _logger = logger;
+    }
 
     public async Task<BlogPorDominioResponse?> Handle(ObterBlogPorDominioQuery request, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Procurando por blog com vinculo com o dominio: {Dominio}", request.Dominio);
         var blogDominio = await _blogDominioRepository.ObterPorNomeDominioAsync(request.Dominio, cancellationToken);
         if (blogDominio?.Blog == null)
             return null;
